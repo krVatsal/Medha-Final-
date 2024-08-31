f38e619dd04ce53b5002f8589d285e3f8d40a316
abhigit-saha <abhijitsaha1006@gmail.com>
2024-08-31 13:45:55 +0530

    removed serveractions: true from nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-      return [
-        {
-          source: '/voiceflow',
-          destination: 'http://localhost:3001/voiceflow',
-        },
-      ];
-    },
-    experimental: {
-      serverActions: true,
-      serverComponentsExternalPackages: ["mongoose"],
-    },
-  };
-  
-  export default nextConfig;
-  
+  async rewrites() {
+    return [
+      {
+        source: "/voiceflow",
+        destination: "http://localhost:3001/voiceflow",
+      },
+    ];
+  },
+  experimental: {
+    // serverActions: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+};
+
+export default nextConfig;
